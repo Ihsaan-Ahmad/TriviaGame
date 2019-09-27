9cc3e665fd94db1e886b7b5522846cc76496378e
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-09-27 14:21:34 -0500

    small update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,190 +1,205 @@
-$("#start").on("click", function() {
-  game.start();
-});
+$(document).ready(function() {
+  var game = {
+    questions: [
+      {
+        question: "What color is the sky?",
+        possibles: ["Blue", "White", "Light Blue", "Black"],
+        id: "question-one",
+        answer: 0
+      },
+      {
+        question: "Are chickens mammals?",
+        possibles: ["Yes", "No", "Kind of", "Not really"],
+        id: "question-two",
+        answer: 1
+      },
+      {
+        question: "Why are whales the largest mammals?",
+        possibles: [
+          "They eat a lot",
+          "They work out",
+          "They were made that way",
+          "They were small before"
+        ],
+        id: "question-three",
+        answer: 2
+      },
+      {
+        question: "Why do javaScript Developers wear glasses?",
+        possibles: [
+          "They are nerds",
+          "They are too smart",
+          "They don't eat carrots",
+          "They don't C#"
+        ],
+        id: "question-four",
+        answer: 3
+      },
+      {
+        question: "What color is a tree?",
+        possibles: [
+          ["Green", "Brown", "Yellow", "Not enough information given"]
+        ],
+        id: "question-five",
+        answer: 3
+      },
+      {
+        question: "How much higher can a human jump than a mountain?",
+        possibles: [["3ft", "1meter", "2km", "Mountains can't jump"]],
+        id: "question-six",
+        answer: 3
+      },
+      {
+        question: "Where are fish from?",
+        possibles: ["Finding Nemo", "The river", "The lighthouse", "The ocean"],
+        id: "question-seven",
+        answer: 3
+      },
+      {
+        question: "What is the capital of Florida?",
+        possibles: [
+          "Miami",
+          "Tampa",
+          "Jacksonville",
+          "Daytona Beach",
+          "Talahassee"
+        ],
+        id: "question-eight",
+        answer: 4
+      },
+      {
+        question: "Why do cows moo?",
+        possibles: [
+          "They feel pain",
+          "They like to growl",
+          "They need to communicate",
+          "God speaks with them",
+          "They are angry"
+        ],
+        id: "question-nine",
+        answer: 2
+      }
+    ]
+  };
 
-$(document).on("click", "#end", function() {
-  game.done();
-});
+  var message = "Game Over!";
 
-var questions = [
-  {
-    question: "What color is the sky?",
-    answers: ["Blue", "White", "Light Blue", "Black"],
-    correctAnswer: ["Blue"]
-  },
-  {
-    question: "Are chickens mammals?",
-    answers: ["Yes", "No", "Kind of", "Not really"],
-    correctAnswer: ["No"]
-  },
-  {
-    question: "Why are whales the largest mammals?",
-    answers: [
-      "They eat a lot",
-      "They work out",
-      "They were made that way",
-      "They were small before"
-    ],
-    correctAnswer: ["They were made that way"]
-  },
-  {
-    question: "Why do javaScript Developers wear glasses?",
-    answers: [
-      "They are nerds",
-      "They are too smart",
-      "They don't eat carrots",
-      "They don't C#"
-    ],
-    correctAnswer: ["They don't C#"]
-  },
-  {
-    question: "What color is a tree?",
-    answers: ["Green", "Brown", "Yellow", "Not enough information given"],
-    correctAnswer: ["Not enough information given"]
-  },
-  {
-    question: "How much higher can a human jump than a mountain?",
-    answers: ["3ft", "1meter", "2km", "Mountains can't jump"],
-    correctAnswer: ["Mountains can't jump"]
-  },
-  {
-    question: "Do birds jump before take-off?",
-    answers: [
-      "A little bit",
-      "Maybe",
-      "They could if they try",
-      "They jump quite a margin"
-    ],
-    correctAnswer: ["A little bit"]
-  },
-  {
-    question: "Where are fish from?",
-    answers: ["Finding Nemo", "The river", "The lighthouse", "The ocean"],
-    correctAnswer: ["The Ocean"]
-  },
-  {
-    question: "Why do you have your wallet/purse?",
-    answers: [
-      "So I can buy things",
-      "So I can stay secure",
-      "So I can look cool",
-      "All of the above"
-    ],
-    correctAnswer: ["All of the above"]
+  $(".startGame").on("click", function() {
+    $(".wrapper").show();
+    console.log("hello");
+
+    $(this).hide();
+  });
+
+  var number = 120;
+  $("#timeLeft").on("click", run);
+
+  function decrement() {
+    number--;
+    $("#timeLeft").html("<h2>" + number + " seconds" + "</h2>");
+    if (number === 0) {
+      stop();
+      $("#message").html("time up!");
+      checkAnswers();
+    }
   }
-];
 
-var game = {
-  correct: 0,
-  incorrect: 0,
-  counter: 120,
-  countdown: function() {
-    game.counter--;
-    $("#counter").html(game.counter);
-    if (game.counter <= 0) {
-      console.log("Time is up!");
-      game.done();
+  function run() {
+    counter = setInterval(decrement, 1000);
+  }
+
+  function stop() {
+    clearInterval(counter);
+  }
+
+  run();
+
+  function formTemplate(data) {
+    var qString = "<form id='questionOne'>" + data.question + "<br>";
+    var possibles = data.possibles;
+    for (var i = 0; i < possibles.length; i++) {
+      var possible = possibles[i];
+      console.log(possible);
+      qString =
+        qString +
+        "<input type='radio' name='" +
+        data.id +
+        "' value=" +
+        i +
+        ">" +
+        possible;
     }
-  },
-  start: function() {
-    timer = setInterval(game.countdown, 1000);
-    $("#subwrapper").prepend(
-      '<h2> Time Remaining: <span id="counter">120</span> Seconds</h2>'
-    );
-    $("#start").remove();
-    for (var i = 0; i < questions.length; i++) {
-      $("#subwrapper").append("<h2>" + questions[i].question + "</h2");
-      for (var j = 0; j < questions[i].answers.length; j++) {
-        $("#subwrapper").append(
-          "<input type= 'radio' name = 'question-' " +
-            i +
-            " ' value='" +
-            questions[i].answers[j] +
-            "'>'" +
-            questions[i].answers[j]
-        );
+    return qString + "</form>";
+  }
+  window.formTemplate = formTemplate;
+
+  function buildQuestions() {
+    var questionHTML = "";
+    for (var i = 0; i < game.questions.length; i++) {
+      questionHTML = questionHTML + formTemplate(game.questions[i]);
+    }
+    $("#questions-container").append(questionHTML);
+  }
+
+  function isCorrect(question) {
+    var answers = $("[name=" + question.id + "]");
+    var correct = answers.eq(question.answer);
+    var isChecked = correct.is(":checked");
+    return isChecked;
+  }
+
+  buildQuestions();
+
+  function resultsTemplate(question) {
+    var htmlBlock = "<div>";
+    htmlBlock = htmlBlock + question.question + ": " + isChecked;
+    return htmlBlock + "</div>";
+  }
+
+  function checkAnswers() {
+    var resultsHTML = "";
+    var guessedAnswers = [];
+    var correct = 0;
+    var incorrect = 0;
+    var unAnswered = 0;
+
+    for (var i = 0; i < game.questions.length; i++) {
+      if (isCorrect(game.questions[i])) {
+        correct++;
+      } else {
+        if (checkAnswered(game.questions[i])) {
+          incorrect++;
+        } else {
+          unAnswered++;
+        }
       }
     }
-    $("#subwrapper").append('<br><button id="end">Done</button>');
-  },
-  done: function() {
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[0].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[1].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[2].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[3].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[4].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[5].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[6].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[7].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-    $.each($('input[name=question-1]":checked'), function() {
-      if ($(this).val() == questions[8].correctAnswer) {
-        game.correct++;
-      } else {
-        game.incorrect++;
-      }
-    });
-
-    this.result();
-  },
-  result: function() {
-    clearInterval(timer);
-    $("#subwrapper h2").remove();
-
-    $("#subwrapper").html("<h2>Finished!</h2>");
-    $("#subwrapper").append("<h3>Correct Answer: " + this.correct + "<h3");
-    $("#subwrapper").append("<h3>Incorrect Answer: " + this.incorrect + "<h3");
-    $("subwrapper").append(
-      "<h3>Unanswered: " +
-        (questions.length - (this.incorrect + this.correct)) +
-        "</h3>"
+    $(".results").html(
+      "correct: " +
+        correct +
+        "<br>" +
+        "incorrect: " +
+        incorrect +
+        "<br>" +
+        "unanswered: " +
+        unAnswered
     );
   }
-};
+
+  function checkAnswered(question) {
+    var anyAnswered = false;
+    var answers = $("[name=" + question.id + "]");
+    for (var i = 0; i < answers.length; i++) {
+      if (answers[i].checked) {
+        anyAnswered = true;
+      }
+    }
+    return anyAnswered;
+  }
+
+  $("#doneButton").on("click", function() {
+    checkAnswers();
+    stop();
+    $("#messageDiv").html("Game Over!");
+  });
+});
